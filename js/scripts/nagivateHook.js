5906f4dca5e84e30c2a2333f02f988c1936de56c
Viktor Liu <17948409+lixmal@users.noreply.github.com>
2026-08-28 02:23:23 +0900

    Upgrade to React Router 7 and import it from the package it lives in now (#350)

--- js/scripts/nagivateHook.js
+++ js/scripts/nagivateHook.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {useLocation, useNavigate} from 'react-router-dom'
+import {useLocation, useNavigate} from 'react-router'
 
 const withNavigateHook = Component => props => {
     const navigate = useNavigate()
