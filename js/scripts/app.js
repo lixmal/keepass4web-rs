5f94222963d96b359e68acb3b3e4570d2b1d99aa
Viktor Liu <viktor@netbird.io>
2026-08-27 18:43:52 +0200

    Upgrade to React Router 7 and import it from the package it lives in now

--- js/scripts/app.js
+++ js/scripts/app.js
@@ -2,7 +2,7 @@ import '../style/app.css'
 import React from 'react'
 import {createRoot} from 'react-dom/client'
 
-import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import {BrowserRouter, Route, Routes} from 'react-router'
 
 import Splash from "./Splash"
 import Viewport from "./Viewport"
