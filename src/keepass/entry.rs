331f88bc86325358ec27adcbebc4ecec9f5430b2
Viktor Liu <viktor@netbird.io>
2026-08-27 21:25:54 +0200

    Carry attachments through a save and send deletions to the recycle bin

--- src/keepass/entry.rs
+++ src/keepass/entry.rs
@@ -1,11 +1,13 @@
 use std::collections::HashMap;
 
-use keepass::db::Value;
+use chrono::NaiveDateTime;
+use keepass::db::EntryRef;
 use regex::Regex;
 use serde::Serialize;
 use uuid::Uuid;
 
 use crate::config::search::{Field, Search};
+use crate::keepass::keepass::{builtin_icon, custom_icon_uuid};
 
 #[derive(Serialize)]
 pub struct Group {
@@ -15,6 +17,7 @@ pub struct Group {
     pub custom_icon_uuid: Option<Uuid>,
     pub children: Vec<Group>,
     pub expanded: bool,
+    pub notes: Option<String>,
 }
 
 #[derive(Serialize)]
@@ -24,6 +27,27 @@ pub struct EntryGroup {
     pub icon: Option<usize>,
     pub custom_icon_uuid: Option<Uuid>,
     pub entries: Vec<Entry>,
+}
+
+// When an entry was created, changed and last looked at. Read-only: the
+// database keeps them, the user does not set them.
+#[derive(Serialize)]
+pub struct Times {
+    pub created: Option<String>,
+    pub modified: Option<String>,
+    pub accessed: Option<String>,
+    pub usage_count: Option<usize>,
+}
+
+// A previous version of an entry, enough of it to tell the versions apart and
+// to see what a field used to hold.
+#[derive(Serialize)]
+pub struct HistoryEntry {
+    pub title: Option<String>,
+    pub username: Option<String>,
+    pub url: Option<String>,
+    pub notes: Option<String>,
+    pub modified: Option<String>,
 }
 
 #[derive(Serialize)]
@@ -39,41 +63,71 @@ pub struct Entry {
     pub custom_icon_uuid: Option<Uuid>,
     pub url: Option<String>,
     pub strings: Option<HashMap<String, Option<String>>>,
+    pub expires: Option<bool>,
+    pub expiry: Option<String>,
+    pub times: Option<Times>,
+    pub history: Option<Vec<HistoryEntry>>,
 }
 
-impl From<&keepass::db::Entry> for Entry {
-    fn from(entry: &keepass::db::Entry) -> Self {
-        let files = vec![];
+fn stamp(time: &Option<NaiveDateTime>) -> Option<String> {
+    time.map(|t| t.and_utc().to_rfc3339())
+}
+
+impl From<&EntryRef<'_>> for Entry {
+    fn from(entry: &EntryRef<'_>) -> Self {
         let mut strings: HashMap<String, Option<String>> = Default::default();
         let mut protected: HashMap<String, ()> = Default::default();
 
         for (k, v) in &entry.fields {
-            match v {
-                Value::Bytes(_) => {}
-                Value::Unprotected(s) => {
-                    strings.insert(k.clone(), Some(s.clone()));
-                }
-                Value::Protected(_) => {
-                    protected.insert(k.clone(), ());
-                    strings.insert(k.clone(), None);
-                }
+            if v.is_protected() {
+                protected.insert(k.clone(), ());
+                strings.insert(k.clone(), None);
+            } else {
+                strings.insert(k.clone(), Some(v.get().clone()));
             }
         }
         strings.remove("Password");
 
+        let files = entry.attachments_named()
+            .map(|(name, _)| name.to_string())
+            .collect();
+
+        let history = entry.history.as_ref().map(|history| {
+            history.get_entries().iter()
+                .map(|old| HistoryEntry {
+                    title: old.get_title().map(String::from),
+                    username: old.get_username().map(String::from),
+                    url: old.get_url().map(String::from),
+                    notes: old.get("Notes").map(String::from),
+                    modified: stamp(&old.times.last_modification),
+                })
+                .collect()
+        });
+
+        let icon = entry.icon().cloned();
+
         // TODO: Don't hide empty protected strings
         Entry {
-            id: entry.uuid,
+            id: entry.id().uuid(),
             title: strings.remove("Title").flatten(),
             username: strings.remove("UserName").flatten(),
             notes: strings.remove("Notes").flatten(),
             binary: Some(files),
             protected: Some(protected),
             tags: Some(entry.tags.clone()),
-            icon: entry.icon_id,
-            custom_icon_uuid: entry.custom_icon_uuid,
+            icon: builtin_icon(&icon),
+            custom_icon_uuid: custom_icon_uuid(&icon),
             url: strings.remove("URL").flatten(),
             strings: Some(strings),
+            expires: entry.times.expires,
+            expiry: stamp(&entry.times.expiry),
+            times: Some(Times {
+                created: stamp(&entry.times.creation),
+                modified: stamp(&entry.times.last_modification),
+                accessed: stamp(&entry.times.last_access),
+                usage_count: entry.times.usage_count,
+            }),
+            history,
         }
     }
 }
@@ -114,5 +168,3 @@ impl Entry {
         false
     }
 }
-
-
